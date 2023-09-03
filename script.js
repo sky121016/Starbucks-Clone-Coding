6aa6a03269f188ca23f130ad2edd746e87266720
sky121016 <sky121016@naver.com>
2023-09-03 19:11:26 +0900

    feat : promotion slider

--- script.js
+++ script.js
@@ -27,4 +27,51 @@ window.onload = function(){
             this.querySelector(".subMenu").style.display = "none";
         });
     });
+
+
+
+    // promotion slider
+    let promoSliderContainer = document.querySelector(".promotion-container");
+    let sliderClone = promoSliderContainer.firstElementChild.cloneNode(true);
+
+    promoSliderContainer.appendChild(sliderClone);
+
+    let currenIndex = 0;
+
+
+    setInterval(() => {
+        currenIndex++;
+
+        promoSliderContainer.style.marginLeft = -currenIndex * 100 + "%";
+        promoSliderContainer.style.transition = "all 0.6s";
+
+        if(currenIndex == 2){
+            setTimeout(() => {
+            promoSliderContainer.style.marginLeft = "0";
+            promoSliderContainer.style.transition = "0s";
+            currenIndex = 0;
+            }, 700);
+        }
+    }, 3000);
+
+    // promo view
+    const promoBtn = document.querySelector("#promotion");
+    const promotionView = document.querySelector(".promotion-view");
+    const promoArrow = document.querySelector("#promotion > span > a");
+    let promoOn = false;
+    promoBtn.addEventListener("click", function(){
+        if(promoOn === false){
+            promotionView.style.display = "block";
+            promoArrow.style.rotate = "180deg";
+            promoOn = true;
+        }else{
+            promotionView.style.display = "none";
+            promoArrow.style.rotate = "180deg";
+            promoOn = false;
+        }
+    });
+
+
+
+
 }
